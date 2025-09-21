2ae0b48bbe708c7f45ef044d98e40c401790a5f2
drvillota <dr.villota@uniandes.edu.co>
2025-09-21 17:17:43 -0500

    Delete old colaboraciones service

--- colaboraciones-servicio/src/modulos/dominio/repositorios.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-from seedwork.dominio.repositorios import Repositorio
-
-
-class RepositorioColaboraciones(Repositorio, ABC):
-    ...
-    
-class RepositorioEventosColaboraciones(Repositorio, ABC):
-    ...
